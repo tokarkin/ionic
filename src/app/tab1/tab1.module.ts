304a15279c3da45511bb70f6f0a3d16890ecbdea
Dima Ksenchyn <42603078+tokarkin@users.noreply.github.com>
2020-05-13 16:42:12 +0300

    ionic help

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,11 +1,18 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonSelectOption, IonSelect } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { PhoneMaskDirective } from './tab1.directive';
+// import { TextMaskModule } from 'angular2-text-mask';
+// import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+// export let options: Partial<IConfig> | (() => Partial<IConfig>);
+// import { InputMaskModule } from 'ionic-input-mask';
+
 
 @NgModule({
   imports: [
@@ -13,8 +20,13 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    ReactiveFormsModule,
+    
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, PhoneMaskDirective],
+  exports: [
+    PhoneMaskDirective
+  ],
 })
 export class Tab1PageModule {}
