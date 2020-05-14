18d3c88c20c518097bf1be8ddad70d92d91fa78a
Dima Ksenchyn <42603078+tokarkin@users.noreply.github.com>
2020-05-14 15:20:05 +0300

    fix

--- src/app/component/search/search.page.ts
+++ src/app/component/search/search.page.ts
@@ -1,19 +1,17 @@
 import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {shareReplay, debounceTime, filter, tap , switchMap, map, catchError, withLatestFrom} from 'rxjs/operators';
+import { debounceTime, tap , map, catchError} from 'rxjs/operators';
 import { Subscription, of } from 'rxjs';
-import { IonSearchbar, NavController, ModalController, Platform } from '@ionic/angular';
+import { IonSearchbar, NavController, Platform } from '@ionic/angular';
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss']
 })
 export class SearchPage implements OnDestroy{
- 
   searchTerm = new FormControl('');
-  customers;
+  customers: any[];
   isLoading = false;
-  // states$ = this.commonOffline.getStates(1, { forceCached: true }).pipe(shareReplay());
   private readonly subscriptions = new Subscription();
   @ViewChild('searchBar', { static: false }) searchBar: IonSearchbar;
   arr = [
@@ -31,14 +29,12 @@ export class SearchPage implements OnDestroy{
   ]
   constructor(
       public navCtrl: NavController,
-      public modalCtrl: ModalController,
       public platform: Platform
   ) {
       const sub = this.searchTerm.valueChanges
           .pipe(
               debounceTime(500),
               tap(() => (this.isLoading = true)),
-              // switchMap(term => this.customerService.getCustomerQuery(term)),
               map(() => {
                 return  this.arr.filter(element => {
                   return element.name.toLowerCase().split(' ').some(word => word.startsWith(this.searchTerm.value.toLowerCase()))
@@ -49,43 +45,17 @@ export class SearchPage implements OnDestroy{
                   this.isLoading = false;
                   return of([]);
               }),
-              // withLatestFrom(this.states$)
           )
           .subscribe((customers: any) => {
             console.log(customers)
-              this.arr = customers
-                  // .filter(f => f.CrmObjectTypeId === CrmObjectTypeEnum.Customers)
-                  // .map(customer => {
-                  //     if (customer.State) {
-                  //         const stateObject = states.find(el => el.Name === customer.State);
-                  //         customer.State = stateObject ? stateObject.Abbreviation : customer.State;
-                  //     }
-                  //     return customer;
-                  // });
+              this.arr = customers;
           });
       this.subscriptions.add(sub);
-      this.subscriptions.add(
-          // this.network.isOnline$.subscribe(online => {
-          //     if (online) {
-          //         this.searchTerm.enable();
-          //     } else {
-          //         this.isLoading = false;
-          //         this.searchTerm.disable();
-          //     }
-          // })
-      );
   }
 
-  // ionViewDidEnter() {
-  //     this.searchBar.setFocus();
-  // }
-
-  selectCustomer(customer: any) {
-      this.searchTerm.setValue(customer.CustomerName, { emitEvent: false });
-      customer.PhoneNumber = customer['Phone'];
-      this.modalCtrl.dismiss(customer);
+  ionViewDidEnter() {
+      this.searchBar.setFocus();
   }
-
   ngOnDestroy() {
       this.subscriptions.unsubscribe();
   }
